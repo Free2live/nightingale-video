be55d5049a8f1e14e8d83a5d0d9e58ea4e57d19b
Thom Allen <thomwork@gmail.com>
2016-01-27 23:18:37 +0000

    Update main.js with oldBrowsersKey init option

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,12 @@
   // Player deferred callback on API Ready
   nightingaleYTDeferred.done(function(YT) {
 
-      // nightingalePlayer.init({
-      //   videoKey: 'aQd41nbQM-U'
-      // });
+    nightingalePlayer.init({
+      videoKey: 'aQd41nbQM-U',
+      oldBrowsersKey: 'blpe_sGnnP4'
+    });
 
-      nightingalePlayer.init();
+    // nightingalePlayer.init();
 
   });
 
