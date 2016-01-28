7be99abde8c4368157d0825e1d49a2df39f80b10
Ben Sharp <bfwsharp@gmail.com>
2016-01-28 16:26:40 +0000

    Add mobile check, remove old player wrapper.

--- scripts/nightingalePlayer.js
+++ scripts/nightingalePlayer.js
@@ -1,24 +1,29 @@
+function mobileCheck() {
+  var check = false;
+  (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))window.location=b;})(navigator.userAgent||navigator.vendor||window.opera, 'http://www.example.com');
+  return check;
+}
+// Check for 360 supported browser
 if(browserCanLoadVideo()){
   // Set player YT Deferred
   var nightingaleYTDeferred = $.Deferred();
   window.onYouTubeIframeAPIReady = function() {
     nightingaleYTDeferred.resolve(window.YT);
   };
-
   // YouTube API ajax call
   $.getScript( "https://www.youtube.com/iframe_api")
     .done(function(script, textStatus) {
   });
 }else{
+  // Display overlay if 360 not supported.
   $('#disabled-overlay').show();
 }
-
 /***************************************************
   Browser sniffage
 
   Since we cannot target browsers for any '360 video enabled' feature,
   and a youtube 360 api is non existent at the moment, user agent sniffing is only answer
-  in this case. Below we are looking for Chrome, FF or Opera (360 supported).
+  in this case. Below we are looking for Chromium browsers, FF or Opera (360 supported).
 
 ***************************************************/
 function browserCanLoadVideo() {
@@ -56,7 +61,6 @@ var nightingalePlayer = (function() {
       // Player settings
       settings = {
         // Define player elements
-        $playerElem: $('#nightingalePlayer__player'),
         $endFrameWrapper: $('#endframe-overlay-wrapper'),
         $playerEndframe: $('#endframe-overlay'),
         $allControlsWrapper: $('.controls'),
@@ -81,6 +85,7 @@ var nightingalePlayer = (function() {
           videoKey: 'Mnf15KwPV-Q'
         },
         // YouTube Api params
+        playerElem: 'nightingalePlayer',
         playerWidth: '100%',
         playerHeight: '100%',
         autoPlay: 1,
@@ -92,6 +97,7 @@ var nightingalePlayer = (function() {
 
         initSettingsChildren: function() {
           // Settings that need sibling reference
+          this.$playerElem =  $('#'+this.playerElem);
           this.$replayVideoBtn = this.$playerEndframe.find('button');
           this.$controlsList = this.$controlsContainer.find('ul');
           this.$volTrack = this.$volContainer.find('.volume__track');
@@ -121,7 +127,7 @@ var nightingalePlayer = (function() {
       if(s.$playerElem.length){
 
         // New up YouTube player with player settings
-        ytp = new YT.Player(s.$playerElem.attr('id'), {
+        ytp = new YT.Player(s.playerElem, {
             width: s.playerWidth,
             height: s.playerHeight,
             // videoId: (browserCanLoadVideo()) ? s.defaults.videoKey : s.defaults.disabledBrowsersKey,
@@ -142,7 +148,7 @@ var nightingalePlayer = (function() {
 
       }else{
         // Throw an error if no target div found.
-        console.error('nightingalePlayer Error: Cannot initialise, no target div found. Please add a div with id "nightingalePlayer__player" to the page.');
+        console.error('nightingalePlayer Error: Cannot initialise, no target div found. Please add a div with id "nightingalePlayer" to the page.');
       }
     }
 
